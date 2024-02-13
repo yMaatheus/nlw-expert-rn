9630a021a235521ea1fff9407af065d54b4961d8
Matheus <ymatheus.h@gmail.com>
2024-02-13 20:56:37 -0300

    feat: :sparkles: component product shows product quantity if exists

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -5,6 +5,7 @@ type ProductDataProps = {
   title: string
   description: string
   thumbnail: ImageProps
+  quantity?: number
 }
 
 type ProductProps = TouchableOpacityProps & {
@@ -18,17 +19,27 @@ export const Product = forwardRef<TouchableOpacity, ProductProps>(({ data, ...re
       className="w-full flex-row items-center pb-4"
       {...rest}
     >
-      <Image source={data.thumbnail} className="w-20 h-20 rounded-md" />
+      <Image
+        source={data.thumbnail}
+        className="w-20 h-20 rounded-md"
+      />
 
       <View className="flex-1 ml-3">
-        <Text
-          className="text-slate-100 font-subtitle text-base flex-1"
-        >
-          {data.title}
-        </Text>
-        <Text
-          className="text-slate-400 text-xs leading-5 mt-0.5"
-        >
+        <View className="flex-row items-center">
+          <Text className="text-slate-100 font-subtitle text-base flex-1">
+            {data.title}
+          </Text>
+
+          {
+            data.quantity && (
+              <Text className="text-slate-400 font-subtitle text-sm">
+                {data.quantity}
+              </Text>
+            )
+          }
+        </View>
+
+        <Text className="text-slate-400 text-xs leading-5 mt-0.5">
           {data.description}
         </Text>
       </View>
