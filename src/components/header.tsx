c0c7793132e7fd3e072e623bcafcc2d654d7251a
Matheus <ymatheus.h@gmail.com>
2024-02-13 20:58:38 -0300

    feat: :sparkles: create cart screen and implements header link to cart screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { Feather } from "@expo/vector-icons";
+import { Link } from "expo-router";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import colors from "tailwindcss/colors";
 
@@ -19,15 +20,17 @@ export function Header({ title, cartQuantityItems = 0 }: HeaderProps) {
 
       {
         cartQuantityItems > 0 && (
-          <TouchableOpacity className="relative" activeOpacity={0.6}>
-            <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
-              <Text className="text-slate-900 font-bold text-xs">
-                {cartQuantityItems}
-              </Text>
-            </View>
+          <Link href="/cart" asChild>
+            <TouchableOpacity className="relative" activeOpacity={0.6}>
+              <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
+                <Text className="text-slate-900 font-bold text-xs">
+                  {cartQuantityItems}
+                </Text>
+              </View>
 
-            <Feather name="shopping-bag" color={colors.white} size={24} />
-          </TouchableOpacity>
+              <Feather name="shopping-bag" color={colors.white} size={24} />
+            </TouchableOpacity>
+          </Link>
         )
       }
     </View>
