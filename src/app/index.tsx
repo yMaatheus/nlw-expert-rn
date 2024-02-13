1cd56d720db120695ae04fee5073a971eb1727ed
Matheus <ymatheus.h@gmail.com>
2024-02-12 22:05:13 -0300

    feat: :sparkles: implements component header and list categories in home screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,37 @@
-import { Text, View } from "react-native"
+import { CategoryButton } from "@/components/category-button"
+import { Header } from "@/components/header"
+import { CATEGORIES } from "@/utils/data/products"
+import { useState } from "react"
+import { FlatList, View } from "react-native"
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-white text-2xl font-heading">React Native!</Text>
+      <Header
+        title="Faça seu pedido"
+        cartQuantityItems={3}
+      />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />)}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   )
 }
