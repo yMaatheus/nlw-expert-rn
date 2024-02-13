7193ab3e95e143b6d18e127ab8c49c9162f73d03
Matheus <ymatheus.h@gmail.com>
2024-02-13 19:45:36 -0300

    fix: :bug: implements redirect to home if product not exists

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -4,7 +4,7 @@ import { useCartStore } from "@/stores/cart-store"
 import { PRODUCTS } from "@/utils/data/products"
 import { formatCurrency } from "@/utils/functions/format-currency"
 import { Feather } from "@expo/vector-icons"
-import { useLocalSearchParams, useNavigation } from "expo-router"
+import { Redirect, useLocalSearchParams, useNavigation } from "expo-router"
 import { Image, Text, View } from "react-native"
 
 export default function Product() {
@@ -12,12 +12,16 @@ export default function Product() {
   const navigation = useNavigation()
   const { id } = useLocalSearchParams()
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0]
+  const product = PRODUCTS.find((item) => item.id === id)
 
   function handleAddToCart() {
+    if (!product) return;
+
     cartStore.add(product)
     navigation.goBack()
   }
+
+  if (!product) return <Redirect href="/" />
 
   return (
     <View className="flex-1">
@@ -27,7 +31,14 @@ export default function Product() {
         resizeMode="cover"
       />
 
+
       <View className="p-5 mt-8 flex-1">
+        <Text
+          className="text-white text-xl font-heading"
+        >
+          {product.title}
+        </Text>
+
         <Text
           className="text-lime-400 text-2xl font-heading my-2"
         >
